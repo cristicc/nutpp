2830eb74dfcff0c508897e58486cc1e44b7e266c
Cristian Ciocaltea <cristian.ciocaltea@gmail.com>
2018-01-30 20:14:02 +0200

    more work

--- src/webserver/main.cpp
+++ src/webserver/main.cpp
@@ -1,28 +1,26 @@
-/*
- * main.cpp
+/**
+ * nutpp - a nutrition planning utility
+ *
+ * Copyright (C) 2018  Cristian Ciocaltea
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @author Cristian Ciocaltea <cristian.ciocaltea@gmail.com>
  */
-
-#include "Connect2UI.h"
-#include "HomeServersConfig.h"
-#include "SystemController.h"
-#include "dbus.h"
 
 #include <Wt/WServer.h>
-
-/*
- * Deployment paths
- */
-#define DEPLOY_PATH_WEBFULL     "/UserInterfaces"
-#define DEPLOY_PATH_WEBLITE     "/BasicInterfaces"
-
-/*
- * Environment variable to be used for enabling application local deployment.
- * That means the application will NOT use the system bus for the communication
- * with the core services, but the session bus.
- * Also a local path will be used for the application configuration data,
- * that is relative to the Wt application root.
- */
-#define ENV_RSG_LOCAL_DEPLOY    "RSG_LOCAL_DEPLOYMENT"
 
 /*
  * Wt logger setup
